204fdebc527cdb4336d391d303d3e47998cc8008
Brian Takawira <brian.takawira@crowngas.co.uk>
2017-06-18 21:01:51 +0100

    check if key value pair exists

--- index.php
+++ index.php
@@ -1,35 +1,48 @@
 <?php
 class Jobs
 {
-    private $jobOutputArray = array();
-    private $joblist = array();
+    private $jobOutput = array();
+    private $jobarray = array();
 
     public function getJobs($jobData)
     {
         if (empty($jobData)) {
             return "Empty sequence.";
         }
-        $job_array = explode("\n", str_replace(" ", '', $jobData)); //converting the string to array
-        foreach ($job_array as $job) {
-            $temp_array = explode("=>", $job);
-            $this->$joblist[trim($temp_array[0])] = trim($temp_array[1]);
+        $jobs = explode("\n", str_replace(" ", '', $jobData)); //convert job data into key value array
+        foreach ($jobs as $job) {
+            $value = explode("=>", $job);
+            $this->jobarray[trim($value[0])] = trim($value[1]);
         }
-        if ($this->checkDependencies($this->joblist) === false) { //check if array has the same value for key and value
+        if ($this->checkDependencies($this->jobarray) === true) { //check if array has the same value for key and value
             return "Jobs can’t depend on themselves.";
+        }
+        foreach ($this->jobarray as $key => $value) {  //check if key value pair exists (if exists - run jobSequen ELSE put key in array)
+            if (empty($value) == 0) {
+                if (!in_array($key, $this->jobOutput)) {
+                    array_push($this->jobOutput, $key);
+                }
+            } else {
+                $this->jobSequence($key, $value);
+            }
         }
     }
 
     private function jobSequence($key, $value)
     {
     }
-    private function checkDependencies($jobArray)
+    private function checkDependencies($keyValueArray)
     {
-        foreach ($jobArray as $key => $value) {
+        foreach ($keyValueArray as $key => $value) {
             if ($key == $value) {
-                return false;
+                return true;
             }
         }
     }
 }
-$job = new Jobs;
-$job->getJobs();
+$n = new Jobs;
+$job = "a => b
+b => c
+c => c
+d => a";
+echo $n->getJobs($job);
