40da6bfda2c89b2192df1edb3e8cad108138170e
Brian Takawira <brian.takawira@crowngas.co.uk>
2017-06-18 08:19:22 +0100

    job dependancy on themselves check

--- index.php
+++ index.php
@@ -9,18 +9,26 @@ class Jobs
         if (empty($jobData)) {
             return "Empty sequence.";
         }
-        $job_array = explode("\n", str_replace(" ", '', $jobData));
+        $job_array = explode("\n", str_replace(" ", '', $jobData)); //converting the string to array
         foreach ($job_array as $job) {
             $temp_array = explode("=>", $job);
             $this->$joblist[trim($temp_array[0])] = trim($temp_array[1]);
+        }
+        if ($this->checkDependencies($this->joblist) === false) { //check if array has the same value for key and value
+            return "Jobs can’t depend on themselves.";
         }
     }
 
     private function jobSequence($key, $value)
     {
     }
-    private function checkDependencies($keyValueArray)
+    private function checkDependencies($jobArray)
     {
+        foreach ($jobArray as $key => $value) {
+            if ($key == $value) {
+                return false;
+            }
+        }
     }
 }
 $job = new Jobs;
